163fe63642a46a67fc152ee7b599b338020fff4c
blackmage <blackmage@magicolic.com>
2012-11-20 20:00:31 -0500

    iipeikou (pure double chii) code & test

--- src/yakupattern-classes.js
+++ src/yakupattern-classes.js
@@ -99,3 +99,29 @@ function Honroutou() {
 Honroutou.prototype = new YakuPattern();
 Honroutou.prototype.constructor = Honroutou;
 
+/**
+ * Iipeikou (pure double chii) yaku pattern
+ * Two chiis of the same value and suit
+ */
+function Iipeikou() {
+    YakuPattern.call(this);
+    
+    this.check = function(hand) {
+        var storedChiis = [];
+        for (var i = 0; i < hand.combinaisons.length; i++) {
+            var combinaison = hand.combinaisons[i];
+            if (combinaison instanceof Chii) {
+                for (var j = 0; j < storedChiis.length; j++) {
+                    if (storedChiis[j].tiles[0].suit == combinaison.tiles[0].suit &&
+                        storedChiis[j].tiles[0].number == combinaison.tiles[0].number) {
+                            return true;
+                    }
+                }
+                storedChiis.push(combinaison);
+            }
+        }
+        return false;
+    };
+}
+Iipeikou.prototype = new YakuPattern();
+Iipeikou.prototype.constructor = Iipeikou;
---
+++ tests/test-yakupattern-iipeikou.js
@@ -0,0 +1,42 @@
+TESTS.push(new TestYakuPatternIipeikou());
+
+function TestYakuPatternIipeikou() {
+    this.runTests = function() {
+        var iipeikou = new Iipeikou();
+        
+        var validHand = new Hand([
+            new Chii(new BambooTile(2), new BambooTile(3), new BambooTile(4)),
+            new Chii(new BambooTile(2), new BambooTile(3), new BambooTile(4)),
+            new Chii(new CharacterTile(3), new CharacterTile(4), new CharacterTile(5)),
+            new Pon(new DotTile(3)),
+            new Pair(new DotTile(7))
+        ]);
+        test("iipeikou (pure double chii) valid hand", function(){
+            ok(iipeikou.check(validHand));
+        });
+        
+        // test with different number
+        var invalidHandWithoutTwoIdenticalChii1 = new Hand([
+            new Chii(new BambooTile(2), new BambooTile(3), new BambooTile(4)),
+            new Chii(new BambooTile(3), new BambooTile(4), new BambooTile(5)),
+            new Chii(new CharacterTile(3), new CharacterTile(4), new CharacterTile(5)),
+            new Pon(new DotTile(3)),
+            new Pair(new DotTile(7))
+        ]);
+        test("iipeikou (pure double chii) invalid hand without two identical chii", function(){
+            ok(iipeikou.check(invalidHandWithoutTwoIdenticalChii1) == false);
+        });
+        
+        // test with different suit
+        var invalidHandWithoutTwoIdenticalChii2 = new Hand([
+            new Chii(new BambooTile(2), new BambooTile(3), new BambooTile(4)),
+            new Chii(new DotTile(2), new DotTile(3), new DotTile(4)),
+            new Chii(new CharacterTile(3), new CharacterTile(4), new CharacterTile(5)),
+            new Pon(new DotTile(3)),
+            new Pair(new DotTile(7))
+        ]);
+        test("iipeikou (pure double chii) invalid hand without two identical chii", function(){
+            ok(iipeikou.check(invalidHandWithoutTwoIdenticalChii2) == false);
+        });
+    };
+}
