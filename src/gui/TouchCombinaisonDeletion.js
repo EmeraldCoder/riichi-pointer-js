6931f372e4409412d785153ed111dca84b4adfb4
EmeraldCoder <la_plume_en_or@hotmail.com>
2014-01-25 13:43:50 -0500

    Touch combinaison deletion
    Enable the combinaison deletion on touch device

---
+++ src/gui/TouchCombinaisonDeletion.js
@@ -0,0 +1,36 @@
+(function ($) {
+
+    var selectionTimeout,
+        selection;
+
+    $('.box-combinaison').on('touchstart', 'span', function(e) {
+        e.preventDefault();
+        
+        if (selectionTimeout) clearTimeout(selectionTimeout);
+        
+        selection = $(this);
+        selectionTimeout = setTimeout(openDeletionConfirmation, 500);
+    });
+    
+    $('.box-combinaison').on('touchend', 'span', function(e) {
+        e.preventDefault();
+        if (selectionTimeout) clearTimeout(selectionTimeout);
+    });
+    
+    function openDeletionConfirmation() {
+        Popup.open(
+            'Do you want to delete this combinaison?', 
+            [
+                { text: 'Delete combinaison', click: deleteSelectedCombinaison }, 
+                { text: 'Cancel' }
+            ]
+        );
+    }
+    
+    function deleteSelectedCombinaison() {           
+        var index = parseInt(selection.attr('data-combinaison-index')),
+            isOpen = selection.parents('div').attr('id') === 'OpenCombinaison';
+        viewModel.remove(index, isOpen);
+    }
+
+} (jQuery));
