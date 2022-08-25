12ed4eb10a77489627a100576ac6458fee89d0d3
EmeraldCoder <la_plume_en_or@hotmail.com>
2022-08-25 19:39:28 -0400

    Update dependencies
    - Vite to 3.x
    - @vitejs/plugin-vue to 3.x
    - Vitest to 0.22.x
    
    and many other sub dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createHtmlPlugin } from 'vite-plugin-html'
